4bd3e4ccbaea053f4ba965647e723e3fc93f6fba
김상 <ksw901106@gmail.com>
2018-07-25 21:48:23 +0900

    로딩씬 프로그레스바, 이미지 옮기는 작업

--- ttippoppotti/WindowAPI/Kim_Sang_Won_addResource.cpp
+++ ttippoppotti/WindowAPI/Kim_Sang_Won_addResource.cpp
@@ -3,9 +3,23 @@
 
 void loadingScene::Kim_Sang_Won_addImage()
 {
+	_loading->loadImage("backGround", "background.bmp", 5755, 2878, true, RGB(255, 0, 255));
+	_loading->loadImage("backGround_rock", "background_rock.bmp", 5755, 2878, true, RGB(255, 0, 255));
+	_loading->loadImage("backGround_object", "background_object.bmp", 5755, 2878, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("helicopter", "helicopter.bmp", 1625, 182, 5, 1);
+	_loading->loadImage("ladder", "ladder.bmp", 25, 237, true, RGB(255, 0, 255));
+
+	_loading->loadFrameImage("saveFlag", "saveFlag.bmp", 3234, 88, 33, 1);
+	_loading->loadFrameImage("humanDead", "human_dead.bmp", 1404, 125, 13, 1);
+	_loading->loadImage("spike", "spike.bmp", 3795.f, 976.f, 15, 108, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("flag", "flag.bmp", 3650.f, 972.f, 2048, 112, 32, 1);
 }
 
 void loadingScene::Kim_Sang_Won_addSound()
 {
-
+	//게임 시작시 3, 2, 1, go!
+	_loading->loadSound("1", "start_one.mp3");
+	_loading->loadSound("2", "start_two.mp3");
+	_loading->loadSound("3", "start_three.mp3");
+	_loading->loadSound("go", "start_go.mp3");
 }
