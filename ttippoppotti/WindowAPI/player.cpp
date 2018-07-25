4a9961b6dba50e89aa0f2609cec290f489a7813f
브룩 <lmk445@naver.com>
2018-07-25 15:39:14 +0900

    플레이어 겟터 셋터 추가

--- ttippoppotti/WindowAPI/player.cpp
+++ ttippoppotti/WindowAPI/player.cpp
@@ -4,6 +4,24 @@
 
 HRESULT player::init(void)
 {
+	_ramBro[IDLE] = IMAGEMANAGER->findImage("broforce_idle");
+	_ramBro[RUN] = IMAGEMANAGER->findImage("broforce_run (1)");
+	//_ramBro[FAST_RUN] = IMAGEMANAGER->addFrameImage("broforce_run (2)", "broforce_run (2).bmp", 448, 128, 8, 2, true, RGB(255, 0, 255));
+	_ramBro[JUMP] = IMAGEMANAGER->findImage("broforce_jump");
+	//_ramBro[ROLL] = IMAGEMANAGER->addFrameImage("broforce_roll", "broforce_roll.bmp", 936, 136, 13, 2, true, RGB(255, 0, 255));
+
+	_state = IDLE;
+	_x = 100.f;
+	_y = 2085.f;
+	_oldX = _x;
+	_oldY = _y;
+	_angle = 90.f *3.14 / 180;
+	_speed = 0.f;
+	_gravity = 0.f;
+	_count = _index = 0;
+	_animationSpeed = 5;
+	_isActived = _isLeft = _isJump = _isFall = false;
+
 	return S_OK;
 }
 
@@ -13,9 +31,12 @@ void player::release(void)
 
 void player::update(void)
 {
+	_gravity += 0.98f;
 
+	FRAMEMANAGER->frameChange(_ramBro[_state], _count, _index, _animationSpeed, _isLeft);
 }
 
 void player::render(void)
 {
+	_ramBro[_state]->frameRender(getMemDC(), _x - CAMERAMANAGER->getCamera().left, _y - CAMERAMANAGER->getCamera().top);
 }
--- ttippoppotti/WindowAPI/playerManager.h
+++ ttippoppotti/WindowAPI/playerManager.h
@@ -5,60 +5,17 @@
 class enemyManager;
 class mapData;
 
-#define G_NUM 0.05f;
-#define S_NUM 23.0f;
-#define SPEED 7
-#define MAX_POSE 5
-
-
-enum pose
-{
-	IDLE, RUN, FAST_RUN, JUMP, ROLL
-};
-
-enum playerState
-{
-	RIGHT_IDLE, LEFT_IDLE,
-	RIGHT_RUN, LEFT_RUN,
-	RIGHT_JUMP, LEFT_JUMP,
-	RIGHT_HANG, LEFT_HANG,
-	LADDER,
-};
-struct tagBroforce
-{
-	RECT rc;
-	float x, y;
-	float speed;
-	float gravity;
-	float angle;
-};
-
-
 class playerManager : public gameNode
 {
 
 private:
 	enemyManager * _enemyManager;
-	player _player;
+	player* _player;
 	mapData* _mapData;
-	image * _ramBro[MAX_POSE];
-
-	playerState _state;
-	tagBroforce _broforce;
-	pose _pose;
+	
 	RECT temp;
-
-	bool _isActived;
-	bool _isLeft1; 
-	bool _isJump; 
-	bool _isFall;
-
-	float _oldX, _oldY;
-	int animationSpeed;
-
-	int _index, _count;		// 프레임 돌릴 변수들
-	int _speed;
-	bool _isLeft;
+	bool hit_left, hit_right;
+	float _ground;
 
 public:
 	void setEnemyManager(enemyManager* enemyManager) { _enemyManager = enemyManager; }
@@ -68,8 +25,6 @@ public:
 	void release(void);
 	void update(void);
 	void render(void);
-
-
 
 	playerManager();
 	~playerManager();
