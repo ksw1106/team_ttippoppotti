30c5f7513fda40b9f1e17570900a7e61a196f71c
5_KGA_14-PC\5_KGA_14 <kor3785@naver.com>
2018-07-26 21:37:46 +0900

    ee

--- ttippoppotti/WindowAPI/enemy.cpp
+++ ttippoppotti/WindowAPI/enemy.cpp
@@ -26,7 +26,9 @@ HRESULT enemy::init(int x, int y, int hp, int randomNum)
 	_warnSign->setFrameX(0);
 	_doubtSign->setFrameY(0);
 		
+	setEnemyAngle(0);
 	setSpeed(3.0f);
+	setGravity(0.05f);
 	setBodyStatus(E_IDLE);
 	setArmStatus(G_IDLE);
 	_frameCount = _frameIndex = _frameIndex2 = _frameIndex3 = _frameIndex4 = 0;
@@ -48,6 +50,11 @@ void enemy::release(void)
 
 void enemy::update(void)
 {
+	if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON))
+	{
+		setBodyStatus(E_FLY_AWAY);		
+	}
+
 	this->controlAI(_randomNumber);
 	this->move();
 	this->frameAnimate();
@@ -58,7 +65,7 @@ void enemy::update(void)
 	_armImage[getArmStatus()]->setX(getEnemyRC().left);
 	_armImage[getArmStatus()]->setY(getEnemyRC().top);
 	
-	setEnemyRC(RectMakeCenter(getX(), getY(), _bodyImage[_enemyStatus]->getFrameWidth(), _bodyImage[_enemyStatus]->getFrameHeight()));
+	setEnemyRC(RectMakeCenter(getX(), getY(), _bodyImage[getBodyStatus()]->getFrameWidth(), _bodyImage[getBodyStatus()]->getFrameHeight()));
 }
 
 void enemy::render(void)
@@ -92,15 +99,26 @@ void enemy::render(void)
 
 void enemy::move()
 {
-	// 좌우 이동
+	//좌우 이동
 	if (getBodyStatus() == E_WALK)
 	{
 		if (_isLeft)
+		{
 			setX(getX() - getSpeed());
-		else 
+			setY(getY() + _gravity);
+		}
+		else
+		{
 			setX(getX() + getSpeed());
-	}	
-	
+			setY(getY() + _gravity);
+		}
+	}
+	//날라갈때
+	if (getBodyStatus() == E_FLY_AWAY)
+	{
+		flyAway();
+	}
+
 	// 적 시야 렉트 변화
 	if (_isLeft)
 	{
@@ -114,27 +132,47 @@ void enemy::move()
 	}	
 }
 
+//날아갈때
+void enemy::flyAway()
+{
+	if (_isLeft)
+	{
+		_angle = PI / 4 * 3;
+		_x += cosf(_angle)*15.0f;
+		_y += -sinf(_angle)*15.0f + _gravity;
+		_gravity += 1.1f;
+	}
+	else
+	{
+		_angle = PI / 4;
+		_x += cosf(_angle)*15.0f;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+		_y += -sinf(_angle)*15.0f + _gravity;
+		_gravity += 1.1f;
+	}
+	//_gravity += 0.05f;
+}
+
 void enemy::controlAI(int randomNum)
 {
 	if (randomNum % 2 == 1)
 	{
 		setBodyStatus(E_WALK);
-		setArmStatus(G_FIRE);
-		setDirection(false);
+		setArmStatus(G_TARGETING);
+		//setDirection(false);
 	}
 	else if (randomNum % 2 == 0)
 	{
 		setBodyStatus(E_IDLE);
+		setArmStatus(G_READY);
 	}
 }
 
 void enemy::frameAnimate()
 {
-	FRAMEMANAGER->frameChange(_bodyImage[getBodyStatus()], _frameCount, _frameIndex, _actionSpeed, _isLeft);
-	FRAMEMANAGER->frameChange(_armImage[getArmStatus()], _frameCount, _frameIndex2, _actionSpeed, _isLeft);
-	FRAMEMANAGER->frameChange(_warnSign, _frameCount, _frameIndex3, _actionSpeed, _isLeft);
-	FRAMEMANAGER->frameChange(_doubtSign, _frameCount, _frameIndex4, _actionSpeed, _isLeft);
-		
+	FRAMEMANAGER->frameChange(_bodyImage[getBodyStatus()], _frameCount, _frameIndex, _actionSpeed, getDirection());
+	//FRAMEMANAGER->frameChange(_armImage[getArmStatus()], _frameCount, _frameIndex2, _actionSpeed, getDirection());
+	FRAMEMANAGER->frameChange(_warnSign, _frameCount, _frameIndex3, _actionSpeed, getDirection());
+	FRAMEMANAGER->frameChange(_doubtSign, _frameCount, _frameIndex4, _actionSpeed, getDirection());
 }
 
 void enemy::knockBackMove()
@@ -142,20 +180,23 @@ void enemy::knockBackMove()
 	// 넉백됐을때, 뒤로 날라감
 	if (_enemyStatus == E_KNOCK_BACK)
 	{
-		setX(getX() - _kbSpeed);
-		_kbSpeed -= 0.5f;
-		if (_kbSpeed < 0)
+		if (_isLeft)
 		{
-			_enemyStatus = E_DEAD;
+			setX(getX() - _kbSpeed);
+			_kbSpeed -= 0.5f;
+			if (_kbSpeed < 0)
+			{
+				_enemyStatus = E_DEAD;
+			}
 		}
-	}	
-	if (_enemyStatus == E_KNOCK_BACK)
-	{
-		setX(getX() + _kbSpeed);
-		_kbSpeed -= 0.5f;
-		if (_kbSpeed < 0)
-		{			
-			_enemyStatus = E_DEAD;
+		else
+		{
+			setX(getX() + _kbSpeed);
+			_kbSpeed -= 0.5f;
+			if (_kbSpeed < 0)
+			{
+				_enemyStatus = E_DEAD;
+			}
 		}
-	}	
+	}		
 }
