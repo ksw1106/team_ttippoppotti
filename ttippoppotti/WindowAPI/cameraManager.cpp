27f4672f52f7689b5891d04d4ae13dc7a6c617c2
김상 <ksw901106@gmail.com>
2018-07-26 13:59:35 +0900

    1스테이지 맵 RECT 완성, 카메라 안에만 렌더 기능 추가

--- ttippoppotti/WindowAPI/cameraManager.cpp
+++ ttippoppotti/WindowAPI/cameraManager.cpp
@@ -1,6 +1,23 @@
 #include "stdafx.h"
 #include "cameraManager.h"
 
+bool cameraManager::CameraIn(RECT rc)
+{
+	if (_rcCamera.left >= rc.right)
+		return true;
+
+	if (_rcCamera.left+WINSIZEY >= rc.left)
+		return true;
+
+	if (_rcCamera.top <= rc.bottom)
+		return true;
+
+	if (_rcCamera.top+WINSIZEY <= rc.top)
+		return true;
+
+	return false;
+}
+
 HRESULT cameraManager::init()
 {
 	return S_OK;
