a55f7e3b127440d2a07053c2080c05e4a8da7a1a
깅밍 <mycielo91@gmail.com>
2018-08-04 01:36:55 +0900

    일단 사진별로 오브젝트 클래스 정리해놓음

--- ttippoppotti/WindowAPI/objectA.h
+++ ttippoppotti/WindowAPI/objectA.h
@@ -2,20 +2,19 @@
 
 enum OBJECT_TYPE
 {
+	SPIKE,
 	SKULL,
 	SKULL_POLE,
-	SKULL_CONTAINER,
+	DOUBLESKULL_POLE,
+	SKULL_DRUM,
 	PRISONER,
-	WOODBOX,
+	WOODENBOX,
 	BOTTLEGREEN,
 	BOTTLEGRAY,
 	BOTTLEBROWN,
 	TRUCK,
-	HUMAN_DEAD,
 	HELICOPTER,
-	BOX,
-	SPIKE,
-	FLAG
+	AMERICAN_FLAG
 };
 
 enum OBJECT_STATE
@@ -31,7 +30,6 @@ class objectA
 protected:
 	OBJECT_TYPE _type;
 	OBJECT_STATE _state;
-	image* _objectImg[3];
 	float _x, _y;
 	float _speed;
 	float _angle;
@@ -68,4 +66,171 @@ private:
 public:
 	image * getHumanDeadImage() { return _humanDeadImage; }
 	image* getSpikeImage() { return _spikeImage; }
+};
+
+class skull : public objectA
+{
+private:
+	image * _skullImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getSkullImage() { return _skullImage; }
+};
+
+class skullPole : public objectA
+{
+private:
+	image * _skullPoleImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getSkullPoleImage() { return _skullPoleImage; }
+};
+
+class doubleSkullPole : public objectA
+{
+private:
+	image * _doubleSkullPoleImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getDoubleSkullPoleImage() { return _doubleSkullPoleImage; }
+};
+
+class skullDrum : public objectA
+{
+private:
+	image * _skullDrumImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getSkullDrumImage() { return _skullDrumImage; }
+};
+
+class prisoner : public objectA
+{
+private:
+	image * _prisonerImage[2];
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getPrisonerImage(int state) { return _prisonerImage[state]; }
+};
+
+class woodenBox : public objectA
+{
+private:
+	image * _woodenBoxImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getWoodenBoxImage() { return _woodenBoxImage; }
+};
+
+class bottleGreen : public objectA
+{
+private:
+	image * _bottleGreenImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getBottleGreenImage() { return _bottleGreenImage; }
+};
+
+class bottleGray : public objectA
+{
+private:
+	image * _bottleGrayImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getBottleGrayImage() { return _bottleGrayImage; }
+};
+
+class bottleBrown : public objectA
+{
+private:
+	image * _bottleBrownImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getBottleBrownImage() { return _bottleBrownImage; }
+};
+
+class truck : public objectA
+{
+private:
+	image * _truckImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getTruckImage() { return _truckImage; }
+};
+
+class helicopter : public objectA
+{
+private:
+	image * _helicopterImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getHelicopterImage() { return _helicopterImage; }
+};
+class americanFlag : public objectA
+{
+private:
+	image * _americanFlagImage;
+private:
+	void init();
+	void idle();
+	void move();
+
+public:
+	image * getAmericanFlagImage() { return _americanFlagImage; }
+};
+
+class factory
+{
+public:
+	virtual objectA* createObject(OBJECT_TYPE) = 0;
+};
+
+class objectFactory : public factory
+{
+public:
+	objectA * createObject(OBJECT_TYPE type);
 };
