fe711c59684beeeafd43041343d78a04dd2cd6cb
깅밍 <mycielo91@gmail.com>
2018-08-03 19:51:35 +0900

    오브젝트 매니저 간보기 / 오브젝트 이미지 작업 / 오브젝트 폴더 생성

--- ttippoppotti/WindowAPI/Kim_Sang_Won_addResource.cpp
+++ ttippoppotti/WindowAPI/Kim_Sang_Won_addResource.cpp
@@ -13,10 +13,10 @@ void loadingScene::Kim_Sang_Won_addImage()
 	_loading->loadImage("backGround_object", "background_object.bmp", 5755, 2878, true, RGB(255, 0, 255));
 	_loading->loadImage("stage2_background_object", "stage2_background_object.bmp", 5582, 2694, true, RGB(255, 0, 255));
 	_loading->loadImage("stage2_background_rock", "stage2_background_rock.bmp", 5582, 2694, true, RGB(255, 0, 255));
-	_loading->loadFrameImage("helicopter", "helicopter.bmp", 1625, 182, 5, 1);
-	_loading->loadImage("ladder", "ladder.bmp", 25, 237, true, RGB(255, 0, 255));
-	_loading->loadFrameImage("saveFlag", "saveFlag.bmp", 3234, 88, 33, 1);
-	_loading->loadFrameImage("flag", "flag.bmp", 3650.f, 972.f, 2048, 112, 32, 1);
+	_loading->loadImage("ladder", "objectImage/ladder.bmp", 25, 237, true, RGB(255, 0, 255));
+	_loading->loadFrameImage("helicopter", "objectImage/helicopter.bmp", 1625, 182, 5, 1);
+	_loading->loadFrameImage("saveFlag", "objectImage/saveFlag.bmp", 3234, 88, 33, 1);
+	_loading->loadFrameImage("flag", "objectImage/flag.bmp", 3650.f, 972.f, 2048, 112, 32, 1);
 }
 
 void loadingScene::Kim_Sang_Won_addSound()
