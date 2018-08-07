b7a93e3e57ab8ebfbb477b2789b435f464761109
5_KGA_14-PC\5_KGA_14 <kor3785@naver.com>
2018-08-07 19:56:11 +0900

    에너미 시체폭발, 움직임 수정

--- ttippoppotti/WindowAPI/enemyManager.h
+++ ttippoppotti/WindowAPI/enemyManager.h
@@ -2,6 +2,8 @@
 #include "gameNode.h"
 #include "bullet.h"
 #include "soldier.h"
+#include "brovil.h"
+#include "dog.h"
 
 class playerManager;
 class mapData;
@@ -12,32 +14,43 @@ private:
 	playerManager* _playerManager;
 	mapData* _mapData;
 	
-	vector<enemy*> _vSoldier;	// 일반 적
-	vector<enemy*> _vBrovil;
+	vector<enemy*> _vSoldier;	// 일반 적	
 		
-	eBullet* _eBullet;			// 총알 클래스
-		
+	eBullet* _eBullet;			// 총알 클래스		
 
 public:
 	void setPlayerManager(playerManager* playerManager) { _playerManager = playerManager; }
 	void setMapData(mapData* mapData) { _mapData = mapData; }
-
+	
 	// 에너미클래스 벡터 가져오기
 	vector<enemy*> getVEnemy() { return _vSoldier; }
+	
 	// 적 총알클래스 총알벡터 가져오기
 	eBullet* getEBullet() { return _eBullet; }
-	
+		
 	HRESULT init(void);
 	void release(void);
 	void update(void);
 	void render(void);
 		
 	void enemyFire(int num);
-	void collision();	// 충돌함수
-	void collideWithPBullet();		// 플레이어 총알과 충돌함수
-	void collideWithPGrenade();		// 플레이어 수류탄과 충돌함수
-	void enemyDie();
+	
+	// 기타 충돌함수
+	void collideWithPixel();
+	// 시체와 픽셀 충돌
+	void collideWithCorpse();
+	// 에너미 시야 vs 플레이어 충돌
+	void collideWithSight();
+	// 에너미 총알 vs 플레이어 충돌
+	void collideWithPlayer();
+	// 플레이어 총알과 충돌함수
+	void collideWithPBullet();	
+	// 플레이어 수류탄과 충돌함수
+	void collideWithPGrenade();		
 
+	void enemyDie();
+	
+	// 솔져 클래스 초기화셋팅
 	void setSoldier(int x, int y);
 	void setBrovil(int x, int y);
 		
